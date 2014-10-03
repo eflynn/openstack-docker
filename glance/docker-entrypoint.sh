be260dc6b5f49853a1f7af35b0ea7b7fa81f4571
Evan Flynn <evan.flynn@gmail.com>
2014-10-03 11:15:12 -0400

    Optimize keystone image

--- glance/docker-entrypoint.sh
+++ glance/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sed -e "s/%RABBITMQ_HOST/$RABBITMQ_PORT_3306_TCP_ADDR/g"
+GLANCE_DB_USER=${GLANCE_DB_USER:-glance}
+GLANCE_DB_NAME=${GLANCE_DB_NAME:-glance}
+
+sed -e "s/%RABBITMQ_HOST%/$RABBITMQ_PORT_3306_TCP_ADDR/g" \
     -e "s/%DB_USER%/$GLANCE_DB_USER/g" \
     -e "s/%DB_PASS%/$GLANCE_DB_PASS/g" \
     -e "s/%DB_NAME%/$GLANCE_DB_NAME/g" \
--- keystone/docker-entrypoint.sh
+++ keystone/docker-entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+if [ -z "$KEYSTONE_DB_PASSWORD" ]; then
+    echo >&2 'error: missing KEYSTONE_DB_PASSWORD environment variable'
+    exit 1
+fi
+
 KEYSTONE_DB_USER=${KEYSTONE_DB_USER:-keystone}
 KEYSTONE_DB_NAME=${KEYSTONE_DB_NAME:-keystone}
 
 sed -e "s/%DB_USER%/$KEYSTONE_DB_USER/g" \
-    -e "s/%DB_PASS%/$KEYSTONE_DB_PASS/g" \
+    -e "s/%DB_PASSWORD%/$KEYSTONE_DB_PASSWORD/g" \
     -e "s/%DB_NAME%/$KEYSTONE_DB_NAME/g" \
     -e "s/%DB_HOST%/$MYSQL_PORT_3306_TCP_ADDR/g" \
-    -i /etc/keystone/keystone.conf
+    -i /etc/keystone.conf
 
 exec "$@"
