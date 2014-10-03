5834b2228ca4bf952eede678ca24d7bfb8ea8c26
Evan Flynn <evan.flynn@gmail.com>
2014-10-03 10:37:53 -0400

    Simplify keystone image

--- keystone/docker-entrypoint.sh
+++ keystone/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+KEYSTONE_DB_USER=${KEYSTONE_DB_USER:-keystone}
+KEYSTONE_DB_NAME=${KEYSTONE_DB_NAME:-keystone}
+
 sed -e "s/%DB_USER%/$KEYSTONE_DB_USER/g" \
     -e "s/%DB_PASS%/$KEYSTONE_DB_PASS/g" \
     -e "s/%DB_NAME%/$KEYSTONE_DB_NAME/g" \
