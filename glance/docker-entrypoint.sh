4c2db7b3bdf0a1455dd9d236bb129e85d7ffcf25
Evan Flynn <evan.flynn@gmail.com>
2014-10-04 14:26:34 -0400

    Add charset

--- glance/docker-entrypoint.sh
+++ glance/docker-entrypoint.sh
@@ -2,12 +2,19 @@
 
 GLANCE_DB_USER=${GLANCE_DB_USER:-glance}
 GLANCE_DB_NAME=${GLANCE_DB_NAME:-glance}
+GLANCE_SERVICE_USER=${GLANCE_SERVICE_USER:-glance}
+SERVICE_TENANT_NAME=${SERVICE_TENANT_NAME:-service}
 
-sed -e "s/%RABBITMQ_HOST%/$RABBITMQ_PORT_3306_TCP_ADDR/g" \
+sed -e "s/%RABBITMQ_HOST%/$RABBITMQ_PORT_5672_TCP_ADDR/g" \
     -e "s/%DB_USER%/$GLANCE_DB_USER/g" \
-    -e "s/%DB_PASS%/$GLANCE_DB_PASS/g" \
+    -e "s/%DB_PASSWORD%/$GLANCE_DB_PASSWORD/g" \
     -e "s/%DB_NAME%/$GLANCE_DB_NAME/g" \
     -e "s/%DB_HOST%/$MYSQL_PORT_3306_TCP_ADDR/g" \
-    -i /etc/keystone/keystone.conf
+    -e "s/%SERVICE_TENANT_NAME%/$SERVICE_TENANT_NAME/g" \
+    -e "s/%SERVICE_USER%/$GLANCE_SERVICE_USER/g" \
+    -e "s/%SERVICE_PASSWORD%/$GLANCE_SERVICE_PASSWORD/g" \
+    -e "s/%KEYSTONE_HOST%/$KEYSTONE_PORT_35357_TCP_ADDR/g" \
+    -i /etc/glance/glance-api.conf \
+    /etc/glance/glance-registry.conf
 
 exec "$@"
